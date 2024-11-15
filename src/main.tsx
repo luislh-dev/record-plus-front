0c786c41c387587fd645ed2d882069a363a67b76
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-15 14:52:25 -0500

    Wrap App component with NextUIProvider for improved styling and theming support

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthProvider';
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthProvider";
+import "./index.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
-        <App />
+        <NextUIProvider>
+          <App />
+        </NextUIProvider>
       </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
