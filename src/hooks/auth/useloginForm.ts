0d37afd8d831f95920822ce04f3ff36308ccae9d
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-15 10:50:56 -0500

    Refactor useLoginForm hook; move implementation to auth directory and remove old user hook

---
+++ src/hooks/auth/useloginForm.ts
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const useLoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const resetForm = () => {
+    setUsername("");
+    setPassword("");
+  };
+
+  return {
+    username,
+    password,
+    setUsername,
+    setPassword,
+    resetForm,
+  };
+};
