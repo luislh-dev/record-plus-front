0eaab43f1a4e6995e8f31bb08ef6ca53b61c5f6a
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-15 16:16:29 -0500

    Update search input placeholder text to Spanish in Hospital component

--- src/pages/Hospital.tsx
+++ src/pages/Hospital.tsx
@@ -18,7 +18,7 @@ export function Hospital() {
             inputWrapper:
               "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
           }}
-          placeholder="Type to search..."
+          placeholder="Escriba para buscar..."
           size="sm"
           startContent={<SearchIcon size={18} />}
           type="search"
