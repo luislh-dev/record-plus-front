51fa10e9b5679f166fcbafe609d241cafb11b759
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-17 23:10:42 -0500

    Remove API URL logging from login function

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,7 +2,6 @@ import { UserAuth } from "../types/userAuth";
 import { api } from "./api/api";
 
 export const login = async (user: UserAuth) => {
-  console.log("API URL:", import.meta.env.VITE_API_URL);
   return await api.post("/login", {
     name: user.username,
     password: user.password,
