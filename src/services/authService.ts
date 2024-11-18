804e85d2df54d050af46d9cec22028d96dc433f4
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-17 23:04:06 -0500

    Log API URL in login function for debugging purposes

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,6 +2,7 @@ import { UserAuth } from "../types/userAuth";
 import { api } from "./api/api";
 
 export const login = async (user: UserAuth) => {
+  console.log("API URL:", import.meta.env.VITE_API_URL);
   return await api.post("/login", {
     name: user.username,
     password: user.password,
