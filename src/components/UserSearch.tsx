831d5c89715926c2adcb41f3dfdfb6951c7935ad
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-17 19:48:02 -0500

    Enhance User interface with improved layout and add navigation button for adding users

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -13,6 +13,13 @@ export const UserSearch = () => {
 
   return (
     <Input
+      classNames={{
+        base: "max-w-full sm:max-w-[15rem] h-10",
+        mainWrapper: "h-full",
+        input: "text-small",
+        inputWrapper:
+          "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+      }}
       type="text"
       value={query}
       onChange={handleSearch}
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,10 +1,23 @@
 import { UserSearch } from "@/components/UserSearch";
 import { UserTable } from "@/components/UserTable";
+import { Button } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
 
 export function User() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <UserSearch />
+      <div className="flex flex-row items-center justify-between mx-3">
+        <h1 className="text-3xl font-bold">Usuarios</h1>
+        <Button color="primary" onClick={() => navigate("/hospital/add")}>
+          Agregar Usuarios
+        </Button>
+      </div>
+      <div className="mx-4 pb-4 pt-3">
+        <UserSearch />
+      </div>
+
       <UserTable />
     </>
   );
