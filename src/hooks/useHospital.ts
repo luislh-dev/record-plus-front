1faca47c04c868f8459da0deebbf9558caf71b9d
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-20 15:31:14 -0500

    Add useHospital hook for managing hospital data fetching and deletion

---
+++ src/hooks/useHospital.ts
@@ -0,0 +1,69 @@
+import { deleteHospital, getHospitals } from "@/services/hospitalService";
+import { HospitalListDTO } from "@/types/DTO/hospital/HospitalListDTO";
+import { HospitalSearchParams } from "@/types/hospital";
+import { useGenericSearch } from "./generic/useGenericSearch";
+import { useCallback, useState } from "react";
+
+interface UseParams {
+  initialPageSize?: number;
+  searchDelay?: number;
+}
+
+export function useHospital(params: UseParams = {}) {
+  const [deleteState, setDeleteState] = useState({
+    isDeleting: false,
+    error: null as string | null,
+  });
+
+  const [modalState, setModalState] = useState({
+    isOpen: false,
+    hospitalId: null as number | null,
+  });
+
+  // Buscar y listar
+  const result = useGenericSearch<HospitalListDTO, HospitalSearchParams>({
+    ...params,
+    fetchData: getHospitals,
+  });
+
+  // Eliminar
+  const handleDelete = useCallback(async () => {
+    if (!modalState.hospitalId) return;
+
+    setDeleteState({ isDeleting: true, error: null });
+
+    try {
+      await deleteHospital(modalState.hospitalId);
+      result.refresh();
+      setModalState({ isOpen: false, hospitalId: null });
+    } catch (error) {
+      console.error("Error deleting hospital:", error);
+      setDeleteState({
+        isDeleting: false,
+        error: "Error al eliminar el hospital",
+      });
+    } finally {
+      setDeleteState((prev) => ({ ...prev, isDeleting: false }));
+    }
+  }, [modalState.hospitalId, result]);
+
+  const openDeleteModal = useCallback((hospitalId: number) => {
+    setModalState({ isOpen: true, hospitalId });
+  }, []);
+
+  const closeDeleteModal = useCallback(() => {
+    setModalState({ isOpen: false, hospitalId: null });
+  }, []);
+
+  return {
+    // listar y buscar
+    ...result,
+
+    // eliminar
+    deleteState,
+    isOpen: modalState.isOpen,
+    openDeleteModal,
+    closeDeleteModal,
+    handleDelete,
+  };
+}
