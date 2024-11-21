5bdc34c96fe3081b4e22302903b7f874cf593d28
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-20 22:36:08 -0500

    Remove unused hospital update and add action files

--- src/actions/hospital/HospitalupdateAction.ts
+++
@@ -1,6 +0,0 @@
-export async function hospitalUpdateAction({ request }: { request: Request }) {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  console.log(data);
-  return null;
-}
--- src/actions/hospital/hospitalAddAction.ts
+++
@@ -1,6 +0,0 @@
-export async function hospitalAddAction({ request }: { request: Request }) {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  console.log(data);
-  return null;
-}
