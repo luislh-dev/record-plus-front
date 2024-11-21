fa627bae8b3c64d02dfcc9ff91f67b3cf704c143
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-21 15:06:20 -0500

    Remove ProtectedRoute component: delete unused file and related code

--- src/routes/ProtectedRoutes.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../contexts/useAuthContext";
-
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const { state } = useAuth();
-  return state.token ? children : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
