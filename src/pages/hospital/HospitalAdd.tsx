bf2dc23372b7f937c2b59e19a075a7696b418bf2
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-21 23:38:10 -0500

    Refactor HospitalAdd component to use HospitalForm for form handling and validation

--- src/pages/hospital/HospitalAdd.tsx
+++ src/pages/hospital/HospitalAdd.tsx
@@ -1,41 +1,17 @@
-import { useStates } from "@/hooks/state/useState";
-import { HospitalCreateRequest } from "@/pages/hospital/types/HospitalCreateRequest";
-import { Button, Input, Select, SelectItem } from "@nextui-org/react";
-import React, { useEffect, useState } from "react";
-import { Form, useNavigate } from "react-router-dom";
+import { Button } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
 import { useHospitalCreate } from "./hooks/useHospitalCreate";
+import { HospitalForm } from "./components/HospitalForm";
+import { HospitalCreateValues } from "./models/hospitalCreateSchema";
 
 export function HospitalAdd() {
   const navigate = useNavigate();
-
-  // Recuperar los estados
-  const state = useStates().state;
-  const [stateValue, setStateValue] = useState<string>("");
-
   const {
     handleCreate,
     createState: { isCreating, error: createError },
   } = useHospitalCreate();
 
-  useEffect(() => {
-    if (state.length > 0 && !stateValue) {
-      setStateValue(state[0].id.toString());
-    }
-  }, [state, stateValue]);
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.currentTarget);
-    const data: HospitalCreateRequest = {
-      name: formData.get("name") as string,
-      ruc: formData.get("ruc") as string,
-      email: formData.get("email") as string,
-      phone: formData.get("phone") as string,
-      address: formData.get("address") as string,
-      stateId: parseInt(stateValue),
-    };
-
+  const onSubmit = async (data: HospitalCreateValues) => {
     try {
       await handleCreate(data);
       navigate("/hospitals");
@@ -53,70 +29,8 @@ export function HospitalAdd() {
         </Button>
       </div>
 
-      <Form
-        method="post"
-        onSubmit={handleSubmit}
-        className="grid grid-cols-2 gap-4"
-      >
-        <Input
-          type="text"
-          name="name"
-          label="Nombre del Hospital"
-          placeholder="Ingrese el nombre"
-          isRequired
-        />
+      <HospitalForm onSubmit={onSubmit} isSubmitting={isCreating} />
 
-        <Input
-          type="text"
-          name="ruc"
-          label="RUC"
-          placeholder="Ingrese el RUC"
-          isRequired
-        />
-
-        <Input
-          type="email"
-          name="email"
-          label="Correo electrónico"
-          placeholder="Ingrese el correo"
-          isRequired
-        />
-
-        <Input
-          type="tel"
-          name="phone"
-          label="Teléfono"
-          placeholder="Ingrese el teléfono"
-          isRequired
-        />
-
-        <Input
-          type="text"
-          name="address"
-          label="Dirección"
-          placeholder="Ingrese la dirección"
-          isRequired
-        />
-        <Select
-          label="Estados"
-          selectedKeys={stateValue ? [stateValue] : []}
-          onSelectionChange={(keys) =>
-            setStateValue(Array.from(keys)[0] as string)
-          }
-        >
-          {state.map((item) => (
-            <SelectItem key={item.id} value={item.id.toString()}>
-              {item.name}
-            </SelectItem>
-          ))}
-        </Select>
-
-        <div className="col-span-2 flex justify-end gap-2">
-          <Button type="submit" color="primary" isLoading={isCreating}>
-            Guardar
-          </Button>
-        </div>
-      </Form>
       {createError && <div className="text-danger">{createError}</div>}
     </div>
   );
---
+++ src/pages/hospital/components/HospitalForm.tsx
@@ -0,0 +1,100 @@
+import { useForm } from "react-hook-form";
+import {
+  HospitalCreateValues,
+  hospitalCreateSchema,
+} from "../models/hospitalCreateSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CustomInput } from "@/components/CustomInput";
+import { Button } from "@nextui-org/react";
+import { CustomSelect } from "@/components/CustomSelect";
+import { useStates } from "@/hooks/state/useState";
+import { useEffect } from "react";
+
+interface Props {
+  onSubmit: (data: HospitalCreateValues) => void;
+  isSubmitting?: boolean;
+}
+
+export const HospitalForm = ({ onSubmit, isSubmitting }: Props) => {
+  const state = useStates().state;
+
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<HospitalCreateValues>({
+    resolver: zodResolver(hospitalCreateSchema),
+    mode: "onChange",
+    defaultValues: {
+      name: "",
+      address: "",
+      phone: "",
+      email: "",
+      ruc: "",
+    },
+  });
+
+  useEffect(() => {
+    if (state.length > 0) {
+      setValue("stateId", state[0].id);
+    }
+  }, [state, setValue]);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-2 gap-4">
+      <CustomInput
+        name="name"
+        control={control}
+        label="Nombre del hospital"
+        placeholder="Ingrese el nombre"
+        error={errors.name}
+        isRequired
+      />
+      <CustomInput
+        name="address"
+        control={control}
+        label="Dirección"
+        placeholder="Ingrese la dirección"
+        error={errors.address}
+        isRequired
+      />
+      <CustomInput
+        name="phone"
+        control={control}
+        label="Teléfono"
+        placeholder="Ingrese el teléfono"
+        error={errors.phone}
+        isRequired
+      />
+      <CustomInput
+        name="email"
+        control={control}
+        label="Correo electrónico"
+        placeholder="Ingrese el correo"
+        error={errors.email}
+        isRequired
+      />
+      <CustomInput
+        name="ruc"
+        control={control}
+        label="RUC"
+        placeholder="Ingrese el RUC"
+        error={errors.ruc}
+        isRequired
+      />
+      <CustomSelect
+        name="stateId"
+        control={control}
+        label="Estado"
+        options={state}
+        error={errors.stateId}
+      />
+      <div className="col-span-2 flex justify-end gap-2">
+        <Button type="submit" color="primary" isLoading={isSubmitting}>
+          Guardar
+        </Button>
+      </div>
+    </form>
+  );
+};
