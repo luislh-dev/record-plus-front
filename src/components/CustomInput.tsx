1d3dffd312fd993d208e2d489256c85ac473882d
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-21 23:37:39 -0500

    Add CustomInput component for enhanced form input handling

---
+++ src/components/CustomInput.tsx
@@ -0,0 +1,51 @@
+import { Input } from "@nextui-org/react";
+import {
+  Control,
+  FieldError,
+  Controller,
+  FieldValues,
+  Path,
+} from "react-hook-form";
+
+interface Props<T extends FieldValues> {
+  name: Path<T>;
+  control: Control<T>;
+  label: string;
+  isRequired?: boolean;
+  placeholder?: string;
+  type?: string;
+  error?: FieldError;
+}
+
+export const CustomInput = <T extends FieldValues>({
+  name,
+  control,
+  label,
+  placeholder,
+  isRequired = false,
+  type,
+  error,
+}: Props<T>) => {
+  return (
+    <div>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <div className="flex flex-col gap-1">
+            <Input
+              {...field}
+              value={field.value?.toString()}
+              type={type}
+              label={label}
+              placeholder={placeholder}
+              isInvalid={!!error}
+              isRequired={isRequired}
+            />
+            <p className="h-4 text-xs text-danger pl-1">{error?.message}</p>
+          </div>
+        )}
+      />
+    </div>
+  );
+};
