5115b2250bf575fe8fbf857ad64d50e21f5b3f51
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-22 16:47:36 -0500

    Add FilterList, Search, and Tune SVG icon components

---
+++ src/icons/Tune.tsx
@@ -0,0 +1,17 @@
+import { iconProps } from "./iconProps";
+
+export function Tune(props: iconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height={props.size || props.height || 20}
+      viewBox="0 -960 960 960"
+      width={props.size || props.width || 20}
+      fill={props.fill || "#64748b"}
+      strokeWidth={props.strokeWidth || 2}
+      {...props}
+    >
+      <path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z" />
+    </svg>
+  );
+}
