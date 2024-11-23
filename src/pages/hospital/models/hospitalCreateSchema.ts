c819f8e26fcefc0f73aa9fe7d6d1f4767bd20216
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-23 16:58:37 -0500

    Add types for hospital creation schema inference and keys extraction

--- src/pages/hospital/models/hospitalCreateSchema.ts
+++ src/pages/hospital/models/hospitalCreateSchema.ts
@@ -25,4 +25,8 @@ export const hospitalCreateSchema = z.object({
   stateId: z.number().min(1, "Estado es requerido"),
 });
 
+// Este tipo es para inferir el tipo de los valores
 export type HospitalCreateValues = z.infer<typeof hospitalCreateSchema>;
+
+// Este tipo es para obtener las claves del objeto
+export type HospitalKeys = keyof z.infer<typeof hospitalCreateSchema>;
