21963c05ea79ce80d6a3d58a27add1b80c5bd4ab
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-11-28 15:04:09 -0500

    Add SEARCH_PARAMS constant for hospital search parameters

---
+++ src/pages/hospital/constants/searchParams.ts
@@ -0,0 +1,7 @@
+import { SearchParam } from "@/types/SearchParam";
+
+export const SEARCH_PARAMS: SearchParam[] = [
+  { id: "name", label: "Nombre" },
+  { id: "ruc", label: "RUC" },
+  { id: "id", label: "ID" },
+];
