b9f6848bdf5e419638ba5f0749652eedb2492a5d
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-05 14:35:57 -0500

    Remove: the search parameter by id

--- src/pages/hospital/constants/searchParams.ts
+++ src/pages/hospital/constants/searchParams.ts
@@ -3,5 +3,4 @@ import { SearchParam } from "@/types/SearchParam";
 export const SEARCH_PARAMS: SearchParam[] = [
   { id: "name", label: "Nombre" },
   { id: "ruc", label: "RUC" },
-  { id: "id", label: "ID" },
 ];
