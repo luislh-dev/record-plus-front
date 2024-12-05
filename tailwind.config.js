47d501bd4fa18e895a1108587b886edcc9d9f4c9
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-05 13:57:16 -0500

    Refactor: convert Tailwind config to ES module syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
-const { nextui } = require("@nextui-org/react");
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [nextui()],
+export const content = [
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
 };
+export const darkMode = "class";
+export const plugins = [nextui()];
