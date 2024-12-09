fae903b72dbcea6dd351f9b09757109bd37a35f5
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-09 13:15:03 -0500

    Agregar: componente UnfoldMore para icono de expansión

---
+++ src/icons/UnfoldMore.tsx
@@ -0,0 +1,15 @@
+import { iconProps } from "./iconProps";
+
+export const UnfoldMore = (props: iconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height={props.size || props.height || 20}
+    viewBox="0 -960 960 960"
+    width={props.size || props.width || 20}
+    fill={props.fill || "#64748b"}
+    strokeWidth={props.strokeWidth || 2}
+    {...props}
+  >
+    <path d="M480-120 300-300l58-58 122 122 122-122 58 58-180 180ZM358-598l-58-58 180-180 180 180-58 58-122-122-122 122Z" />
+  </svg>
+);
