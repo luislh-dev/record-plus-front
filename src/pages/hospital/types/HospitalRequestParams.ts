49be59d0dfd21783341f550d61c3ec8ee7a58a37
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-09 13:14:48 -0500

    Agregar: interfaz HospitalRequestParams para parámetros de solicitud de hospital

---
+++ src/pages/hospital/types/HospitalRequestParams.ts
@@ -0,0 +1,9 @@
+import { PageRequest } from "@/types/page/PageRequest";
+
+export interface HospitalRequestParams extends PageRequest {
+  name?: string;
+  ruc?: string;
+  id?: number;
+  stateId?: number;
+  sort?: string;
+}
