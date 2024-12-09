4f858a15b044d87abf965ab7184dfeea70b473b1
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-09 16:19:10 -0500

    Actualizar: envolver la aplicación en QueryClientProvider para integrar react-query

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import { AuthProvider } from "./contexts/AuthProvider";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
