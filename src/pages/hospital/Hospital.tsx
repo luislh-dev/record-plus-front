2d3ab60fc8985761b0e5887a6e479771dde275b8
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-10 17:31:50 -0500

    Agregar: implementar componente de búsqueda reutilizable y simplificar la barra de búsqueda en Hospital

--- src/pages/hospital/Hospital.tsx
+++ src/pages/hospital/Hospital.tsx
@@ -3,23 +3,22 @@ import { ModalConfirmDelete } from "@/components/ModalConfirmDelete";
 import { statusColorMap } from "@/constants/statusColorMap";
 import { useHospital } from "@/pages/hospital/hooks/useHospital";
 import { Add } from "@/icons/Add";
-import { Button, Chip, Input } from "@nextui-org/react";
+import { Button, Chip } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import { HospitalListDTO } from "./types/HospitalListDTO";
 import { HospitalTableColumn } from "./types/hospital";
 import { ActionsCell } from "./components/ActionCells";
 import { DropDownFilter } from "./components/DropDrownFilter";
 import { DropDownSort } from "./components/DropDownSort";
-import { Search } from "@/icons/Search";
 import { SearchParamsDropdown } from "./components/DropDownSearchParams";
 import { Align } from "@/constants/align";
 import { useSearchStore } from "./stores/searchStore";
+import { SearchImput } from "./components/Search";
 
 const Hospital = () => {
   const navigate = useNavigate();
 
-  const { searchTerm, setSearchTerm, setPage, sortConfig, setSortConfig } =
-    useSearchStore();
+  const { setPage, sortConfig, setSortConfig } = useSearchStore();
 
   // Obtener toda la funcionalidad del hook
   const {
@@ -93,21 +92,7 @@ const Hospital = () => {
 
         {/* Barra de búsqueda y filtros */}
         <search className="px-2 pb-2 pt-4 flex gap-x-4">
-          <Input
-            classNames={{
-              base: "max-w-full sm:max-w-[15rem] h-10",
-              mainWrapper: "h-full",
-              input: "text-small",
-              inputWrapper:
-                "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-            }}
-            onClear={() => setSearchTerm("")}
-            type="text"
-            placeholder="Buscar hospital..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            startContent={<Search />}
-          />
+          <SearchImput />
           <SearchParamsDropdown />
           <DropDownFilter />
           <DropDownSort />
---
+++ src/pages/hospital/components/Search.tsx
@@ -0,0 +1,25 @@
+import { Input } from "@nextui-org/react";
+import { useSearchStore } from "../stores/searchStore";
+import { Search } from "@/icons/Search";
+
+export const SearchImput = () => {
+  const { searchTerm, setSearchTerm } = useSearchStore();
+
+  return (
+    <Input
+      classNames={{
+        base: "max-w-full sm:max-w-[15rem] h-10",
+        mainWrapper: "h-full",
+        input: "text-small",
+        inputWrapper:
+          "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+      }}
+      onClear={() => setSearchTerm("")}
+      type="text"
+      placeholder="Buscar hospital..."
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      startContent={<Search />}
+    />
+  );
+};
