06d009c5783135fd0769a3080430561630faaf3b
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-10 17:38:22 -0500

    Refactor: extraer la tabla de hospitales a un componente separado y simplificar el componente Hospital

--- src/pages/hospital/Hospital.tsx
+++ src/pages/hospital/Hospital.tsx
@@ -1,88 +1,26 @@
-import { GenericTable } from "@/components/GenericTable";
 import { ModalConfirmDelete } from "@/components/ModalConfirmDelete";
-import { statusColorMap } from "@/constants/statusColorMap";
 import { useHospital } from "@/pages/hospital/hooks/useHospital";
 import { Add } from "@/icons/Add";
-import { Button, Chip } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
-import { HospitalListDTO } from "./types/HospitalListDTO";
-import { HospitalTableColumn } from "./types/hospital";
-import { ActionsCell } from "./components/ActionCells";
 import { DropDownFilter } from "./components/DropDrownFilter";
 import { DropDownSort } from "./components/DropDownSort";
 import { SearchParamsDropdown } from "./components/DropDownSearchParams";
-import { Align } from "@/constants/align";
-import { useSearchStore } from "./stores/searchStore";
 import { SearchImput } from "./components/Search";
+import { HospitalList } from "./components/HospitalList";
 
 const Hospital = () => {
   const navigate = useNavigate();
 
-  const { setPage, sortConfig, setSortConfig } = useSearchStore();
-
   // Obtener toda la funcionalidad del hook
   const {
-    hospitals,
-    isLoading,
-    error,
-    pagination,
     deleteState: { isDeleting },
     isOpen,
-    openDeleteModal,
     closeDeleteModal,
     handleDelete,
   } = useHospital();
 
   // Definición de columnas para la tabla
-  const columns: HospitalTableColumn[] = [
-    {
-      name: "Nombre",
-      uuid: "name",
-      sortable: true,
-    },
-    {
-      name: "Teléfono",
-      uuid: "phone",
-      sortable: true,
-    },
-    {
-      name: "Correo electrónico",
-      uuid: "email",
-      sortable: true,
-    },
-    {
-      name: "RUC",
-      uuid: "ruc",
-      sortable: true,
-    },
-    {
-      name: "Estado",
-      uuid: "state",
-      align: Align.CENTER,
-      render: (hospital: HospitalListDTO) => (
-        <Chip
-          className="capitalize"
-          color={statusColorMap[hospital.state] || "default"}
-          size="sm"
-          variant="flat"
-        >
-          {hospital.state}
-        </Chip>
-      ),
-    },
-    {
-      name: "Acciones",
-      uuid: "actions",
-      align: Align.CENTER,
-      render: (hospital: HospitalListDTO) => (
-        <ActionsCell
-          hospital={hospital}
-          onEdit={() => navigate(`/hospitals/${hospital.id}/edit`)}
-          onDelete={() => openDeleteModal(hospital.id)}
-        />
-      ),
-    },
-  ];
 
   return (
     <>
@@ -98,21 +36,7 @@ const Hospital = () => {
           <DropDownSort />
         </search>
         {/* Tabla de hospitales */}
-        <GenericTable
-          columns={columns}
-          data={hospitals}
-          error={error?.message}
-          emptyMessage="No se encontraron hospitales."
-          isLoading={isLoading}
-          loadingContent="Cargando hospitales..."
-          totalPages={pagination.totalPages}
-          currentPage={pagination.currentPage}
-          onPageChange={setPage}
-          onSort={(field) =>
-            setSortConfig({ field, direction: sortConfig.direction })
-          }
-          sortConfig={sortConfig}
-        />
+        <HospitalList />
       </div>
 
       {/* Modal de confirmación para eliminar */}
---
+++ src/pages/hospital/components/HospitalList.tsx
@@ -0,0 +1,93 @@
+import { GenericTable } from "@/components/GenericTable";
+import { HospitalListDTO } from "../types/HospitalListDTO";
+import { Align } from "@/constants/align";
+import { statusColorMap } from "@/constants/statusColorMap";
+import { Chip } from "@nextui-org/react";
+import { HospitalTableColumn } from "../types/hospital";
+import { ActionsCell } from "./ActionCells";
+import { useNavigate } from "react-router-dom";
+import { useHospital } from "../hooks/useHospital";
+import { useSearchStore } from "../stores/searchStore";
+
+export const HospitalList = () => {
+  const navigate = useNavigate();
+
+  const { setPage, sortConfig, setSortConfig } = useSearchStore();
+
+  const {
+    hospitals,
+    isLoading,
+    error,
+    pagination,
+
+    openDeleteModal,
+  } = useHospital();
+
+  const columns: HospitalTableColumn[] = [
+    {
+      name: "Nombre",
+      uuid: "name",
+      sortable: true,
+    },
+    {
+      name: "Teléfono",
+      uuid: "phone",
+      sortable: true,
+    },
+    {
+      name: "Correo electrónico",
+      uuid: "email",
+      sortable: true,
+    },
+    {
+      name: "RUC",
+      uuid: "ruc",
+      sortable: true,
+    },
+    {
+      name: "Estado",
+      uuid: "state",
+      align: Align.CENTER,
+      render: (hospital: HospitalListDTO) => (
+        <Chip
+          className="capitalize"
+          color={statusColorMap[hospital.state] || "default"}
+          size="sm"
+          variant="flat"
+        >
+          {hospital.state}
+        </Chip>
+      ),
+    },
+    {
+      name: "Acciones",
+      uuid: "actions",
+      align: Align.CENTER,
+      render: (hospital: HospitalListDTO) => (
+        <ActionsCell
+          hospital={hospital}
+          onEdit={() => navigate(`/hospitals/${hospital.id}/edit`)}
+          onDelete={() => openDeleteModal(hospital.id)}
+        />
+      ),
+    },
+  ];
+
+  return (
+    <GenericTable
+      columns={columns}
+      data={hospitals}
+      error={error?.message}
+      emptyMessage="No se encontraron hospitales."
+      isLoading={isLoading}
+      loadingContent="Cargando hospitales..."
+      totalPages={pagination.totalPages}
+      currentPage={pagination.currentPage}
+      onPageChange={setPage}
+      onSort={(field) =>
+        setSortConfig({ field, direction: sortConfig.direction })
+      }
+      sortConfig={sortConfig}
+    />
+  );
+};
