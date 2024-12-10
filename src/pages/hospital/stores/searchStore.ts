a0b4ee149148650da1a12150b8a1de9669d716ab
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-10 17:07:24 -0500

    Agregar: implementar store de búsqueda para gestionar términos de búsqueda, configuración de ordenamiento y filtros en el estado

---
+++ src/pages/hospital/stores/searchStore.ts
@@ -0,0 +1,87 @@
+import { SortConfig, SortDirection } from "@/types/sorting";
+import { HospitalRequestParams } from "../types/HospitalRequestParams";
+import { create } from "zustand";
+import { SEARCH_PARAMS } from "../constants/searchParams";
+
+interface SearchState {
+  // Estado
+  searchTerm: string;
+  sortConfig: SortConfig;
+  selectedState: number | null;
+  searchFields: string[];
+  filters: HospitalRequestParams;
+
+  // Acciones
+  setSearchTerm: (searchTerm: string) => void;
+  setSortConfig: (config: SortConfig) => void;
+  setSelectedState: (stateId: number | null) => void;
+  setSearchFields: (fields: string[]) => void;
+  setFilters: (filters: HospitalRequestParams) => void;
+  setPage: (page: number) => void;
+  setPageSize: (size: number) => void;
+
+  // Utilidades
+  buildSearchParams: () => HospitalRequestParams;
+}
+
+export const useSearchStore = create<SearchState>((set, get) => ({
+  // Estado inicial
+  searchTerm: "",
+  sortConfig: { field: "updatedAt", direction: SortDirection.DESC },
+  selectedState: null,
+  searchFields: [SEARCH_PARAMS[0].id],
+  filters: { page: 0, size: 20 },
+
+  // Acciones
+  setSearchTerm: (term) =>
+    set({
+      searchTerm: term,
+      filters: { ...get().filters, page: 0 },
+    }),
+
+  setSortConfig: (config) => set({ sortConfig: config }),
+
+  setSelectedState: (stateId) =>
+    set((state) => ({
+      selectedState: stateId,
+      filters: {
+        ...state.filters,
+        stateId: stateId ?? undefined,
+      },
+    })),
+
+  setSearchFields: (fields) => set({ searchFields: fields }),
+
+  setFilters: (filters) => set({ filters }),
+
+  setPage: (page) => set({ filters: { ...get().filters, page } }),
+
+  setPageSize: (size) => set({ filters: { ...get().filters, size } }),
+
+  // Utilidades
+  // Función para construir los parámetros de búsqueda
+  buildSearchParams: () => {
+    const state = get();
+    const params: HospitalRequestParams = {
+      ...state.filters,
+      sort: `${state.sortConfig.field},${state.sortConfig.direction}`,
+    };
+
+    if (state.searchTerm) {
+      state.searchFields.forEach((field) => {
+        if (SEARCH_PARAMS.some((param) => param.id === field)) {
+          if (field === "id") {
+            const numericId = parseInt(state.searchTerm);
+            if (!isNaN(numericId)) {
+              params.id = numericId;
+            }
+          } else if (field === "ruc" || field === "name") {
+            params[field] = state.searchTerm;
+          }
+        }
+      });
+    }
+
+    return params;
+  },
+}));
