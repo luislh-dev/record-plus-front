183fb2405c9a9bef735e75591a2fc5cb5fbef1b6
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-11 11:01:49 -0500

    Actualizar componente Hospital: integrar Header y eliminar lógica de navegación para agregar hospital

--- src/pages/hospital/Hospital.tsx
+++ src/pages/hospital/Hospital.tsx
@@ -1,16 +1,12 @@
 import { ModalConfirmDelete } from "@/components/ModalConfirmDelete";
 import { useHospital } from "@/pages/hospital/hooks/useHospital";
-import { Add } from "@/icons/Add";
-import { Button } from "@nextui-org/react";
-import { useNavigate } from "react-router-dom";
 import { DropDownFilter } from "./components/DropDrownFilter";
 import { DropDownSort } from "./components/DropDownSort";
 import { SearchImput } from "./components/Search";
 import { HospitalList } from "./components/HospitalList";
+import { Header } from "./components/Header";
 
 const Hospital = () => {
-  const navigate = useNavigate();
-
   // Obtener toda la funcionalidad del hook
   const {
     deleteState: { isDeleting },
@@ -25,7 +21,7 @@ const Hospital = () => {
     <>
       <div>
         {/* Encabezado */}
-        <Header onAddHospital={() => navigate("/hospitals/add")} />
+        <Header />
 
         {/* Barra de búsqueda y filtros */}
         <search className="px-2 pb-2 pt-4 flex gap-x-4">
@@ -49,26 +45,5 @@ const Hospital = () => {
     </>
   );
 };
-
-// Componentes auxiliares para mejorar la legibilidad
-
-interface HeaderProps {
-  onAddHospital: () => void;
-}
-
-function Header({ onAddHospital }: HeaderProps) {
-  return (
-    <div className="flex max-w-full justify-between px-3">
-      <h1 className="text-2xl font-bold">Lista de Hospitales</h1>
-      <Button
-        color="primary"
-        onClick={onAddHospital}
-        endContent={<Add size={18} />}
-      >
-        Agregar Hospital
-      </Button>
-    </div>
-  );
-}
 
 export default Hospital;
