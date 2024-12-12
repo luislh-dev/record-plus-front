82e9e39a961835f84527d04d465f35b641478cc0
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-11 21:24:18 -0500

    Agregar soporte genérico para la gestión de ordenamiento en useHandleSort

--- src/hooks/useHandleSort.ts
+++ src/hooks/useHandleSort.ts
@@ -1,4 +1,4 @@
-import { SortConfig, SortDirection } from "@/types/sorting";
+import { SortConfig, SortConfigGeneric, SortDirection } from "@/types/sorting";
 
 export const useHandleSort = (sortConfig: SortConfig) => {
   const getNextSortDirection = (field: string): SortDirection => {
@@ -9,6 +9,25 @@ export const useHandleSort = (sortConfig: SortConfig) => {
   };
 
   const getNewSortConfig = (field: string): SortConfig => ({
+    field,
+    direction: getNextSortDirection(field),
+  });
+
+  return {
+    getNextSortDirection,
+    getNewSortConfig,
+  };
+};
+
+export const useHandleSortGeneric = <T>(sortConfig: SortConfigGeneric<T>) => {
+  const getNextSortDirection = (field: T): SortDirection => {
+    return sortConfig.field === field &&
+      sortConfig.direction === SortDirection.ASC
+      ? SortDirection.DESC
+      : SortDirection.ASC;
+  };
+
+  const getNewSortConfig = (field: T): SortConfigGeneric<T> => ({
     field,
     direction: getNextSortDirection(field),
   });
