706a8e27d14e78ee63adfc901204648f39f56b7b
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-12 08:35:34 -0500

    Agregar componente de búsqueda en User para filtrar usuarios

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,8 +1,12 @@
+import { Search } from "./components/Search";
 import { UserList } from "./components/UserList";
 
 const User = () => {
   return (
     <>
+      <nav>
+        <Search />
+      </nav>
       <UserList />
     </>
   );
