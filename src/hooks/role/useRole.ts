3f8f8463f7f894262307ecaf1403667680ccc8ad
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-12 15:04:05 -0500

    Agregar hook useRole para obtener y gestionar roles desde el servicio de roles

---
+++ src/hooks/role/useRole.ts
@@ -0,0 +1,16 @@
+import { getRoles } from "@/services/rolesService";
+import { useQuery } from "@tanstack/react-query";
+
+export const useRole = () => {
+  const {
+    data: roles,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["roles"],
+    queryFn: async () => getRoles(),
+    staleTime: 1000 * 60 * 60 * 24, // 24 horas
+  });
+
+  return { roles: roles ?? [], isLoading, error };
+};
