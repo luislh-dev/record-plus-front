d108373047f8e9ea94f58d6e4ba5dd98601d0ca9
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-14 21:23:40 -0500

    Agregar componente Header y reorganizar estructura en la página de usuarios

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,15 +1,19 @@
 import { DropDownFilter } from "./components/DropDownFilter";
+import { Header } from "./components/Header";
 import { Search } from "./components/Search";
 import { UserList } from "./components/UserList";
 
 const User = () => {
   return (
     <>
-      <nav className="flex gap-x-4 px-2 pb-2 pt-4">
-        <Search />
-        <DropDownFilter />
-      </nav>
-      <UserList />
+      <section>
+        <Header />
+        <nav className="flex gap-x-4 px-2 pb-2 pt-4">
+          <Search />
+          <DropDownFilter />
+        </nav>
+        <UserList />
+      </section>
     </>
   );
 };
---
+++ src/pages/user/components/Header.tsx
@@ -0,0 +1,19 @@
+import { Add } from "@/icons/Add";
+import { Button } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
+
+export const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="flex max-w-full justify-between px-3">
+      <h1 className="text-2xl font-bold">Lista de Usuarios</h1>
+      <Button
+        color="primary"
+        onClick={() => navigate("/user/add")}
+        endContent={<Add size={18} />}
+      >
+        Agregar Usuario
+      </Button>
+    </div>
+  );
+};
