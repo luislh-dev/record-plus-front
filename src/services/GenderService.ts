9fecebb53c2b54164f58eaacdf1b52af0627031a
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-18 16:23:47 -0500

    Add: implementar servicio para obtener tipos de género y definir interfaz Gender

---
+++ src/services/GenderService.ts
@@ -0,0 +1,7 @@
+import { Gender } from "@/types/Gender";
+import { api } from "./api/api";
+
+export const getGender = async (): Promise<Gender[]> => {
+  const response = await api.get<Gender[]>(`/sexType`);
+  return response.data;
+};
