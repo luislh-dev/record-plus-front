15bf2ab3024500bd9050375c4806b2d9d9d1ce78
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-18 14:44:17 -0500

    Add: crear función toManagementCreationDto para transformar valores de creación de usuario en ManagementCreationDto

---
+++ src/pages/user/adapter/toManagementCreationDto.ts
@@ -0,0 +1,13 @@
+import { UserManagementCreateValues } from "../models/userManagementCreateSchema";
+import { ManagementCreationDto } from "../types/ManagementCreationDto";
+
+export const toManagementCreationDto = (
+  values: UserManagementCreateValues,
+): ManagementCreationDto => ({
+  name: values.name,
+  email: values.email,
+  password: values.password,
+  personDNI: values.personDNI,
+  stateId: values.stateId,
+  hospitalId: values.hospitalId,
+});
