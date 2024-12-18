c263bdf424e5ff2f2271a5c1f998d99853c95b96
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-18 17:00:06 -0500

    Add: configurar QueryClient y persistencia de queries en localStorage

---
+++ src/config/queryClient.ts
@@ -0,0 +1,24 @@
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { QueryClient } from "@tanstack/react-query";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+
+export const queryClient = new QueryClient();
+
+// Crear el persistor
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
+// Configurar la persistencia
+persistQueryClient({
+  queryClient,
+  persister,
+  // Puedes especificar qué queries quieres persistir
+  dehydrateOptions: {
+    shouldDehydrateQuery: (query) => {
+      // Solo persistir queries específicas
+      const persistentQueries = ["states", "documentType"];
+      return persistentQueries.includes(query.queryKey[0] as string);
+    },
+  },
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { queryClient } from "./config/queryClient";
 import { AuthProvider } from "./contexts/AuthProvider";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
-const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
@@ -17,5 +17,5 @@ root.render(
         <RouterProvider router={router} />
       </AuthProvider>
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
