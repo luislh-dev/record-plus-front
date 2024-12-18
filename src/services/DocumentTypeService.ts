7430f12357ab5acfb1517b84e64ba11347a8e44e
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-18 16:23:58 -0500

    Add: implementar servicio para obtener tipos de documento y definir interfaz DocumentType

---
+++ src/services/DocumentTypeService.ts
@@ -0,0 +1,7 @@
+import { DocumentType } from "@/types/DocumentType";
+import { api } from "./api/api";
+
+export const getDocumentTypes = async (): Promise<DocumentType[]> => {
+  const response = await api.get<DocumentType[]>("/documentType");
+  return response.data;
+};
---
+++ src/types/DocumentType.ts
@@ -0,0 +1,3 @@
+import { BaseEntity } from "./BaseEntity";
+
+export interface DocumentType extends BaseEntity {}
