38d29d243cbaf10e7de2167f76d83c94e9d24863
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-18 22:27:56 -0500

    Add: implementar función para crear usuarios de gestión en el servicio de usuario

--- src/pages/user/service/userService.ts
+++ src/pages/user/service/userService.ts
@@ -1,5 +1,6 @@
 import { api } from "@/services/api/api";
 import { PageResponse } from "@/types/Pagination";
+import { ManagementCreationDto } from "../types/ManagementCreationDto";
 import { UserListDTO } from "../types/UserListDTO";
 import { UserRequestParams } from "../types/UserRequestParams";
 
@@ -12,4 +13,9 @@ export const getUsers = async (params?: UserRequestParams): Promise<PageResponse
 
 export const deleteUser = async (id: string): Promise<void> => {
   await api.delete(`/users/${id}`);
+};
+
+export const createManagementUser = async (data: ManagementCreationDto) => {
+  const response = await api.post<ManagementCreationDto>("/users/createManagementUser", data);
+  return response.data;
 };
