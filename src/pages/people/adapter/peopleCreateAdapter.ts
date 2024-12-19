cef312168d0322ae35c41304cddcd70cf2eb2db8
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-18 22:27:48 -0500

    Add: crear adaptador para convertir datos requeridos de creación de personas a DTO completo

---
+++ src/pages/people/adapter/peopleCreateAdapter.ts
@@ -0,0 +1,15 @@
+import { PeopleCreateRequiredValues } from "../models/peopleCreateRequiredSchema";
+import { PeopleCreateFullDto } from "../types/PeopleCreateFullDto";
+
+export const toPeopleRequiredDto = (data: PeopleCreateRequiredValues): PeopleCreateFullDto => {
+  return {
+    name: data.name,
+    paternalSurname: data.paternalSurname,
+    maternalSurname: data.maternalSurname,
+    phone: data.phone ?? "",
+    typeDocumentId: data.typeDocumentId,
+    documentNumber: data.documentNumber,
+    birthdate: data.birthdate,
+    sexTypeId: data.sexTypeId,
+  };
+};
