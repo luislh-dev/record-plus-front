275ce059817323809a17e908eb0aedaee13d12e2
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-18 22:28:21 -0500

    Add: implementar hook useCreateRequiredPerson para gestionar la creación de personas

---
+++ src/pages/people/hooks/useCreatePerson.ts
@@ -0,0 +1,28 @@
+import { ApiServiceError } from "@/services/api/ApiErrorHandler";
+import { useState } from "react";
+import { toPeopleRequiredDto } from "../adapter/peopleCreateAdapter";
+import { PeopleCreateRequiredValues } from "../models/peopleCreateRequiredSchema";
+import { createPerson } from "../services/peopleService";
+
+export function useCreateRequeridPerson() {
+  const [isCreating, setIsCreating] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleCreate = async (data: PeopleCreateRequiredValues) => {
+    setIsCreating(true);
+    setError(null);
+
+    try {
+      await createPerson(toPeopleRequiredDto(data));
+    } catch (error) {
+      if (error instanceof ApiServiceError) {
+        setError(error.error.message);
+      }
+      throw error;
+    } finally {
+      setIsCreating(false);
+    }
+  };
+
+  return { isCreating, error, handleCreate };
+}
--- src/pages/people/services/peopleService.ts
+++ src/pages/people/services/peopleService.ts
@@ -1,7 +1,12 @@
 import { api } from "@/services/api/api";
 import { MinimalPeopleResponseDto } from "../types/MinimalPeopleResponseDto";
+import { PeopleCreateFullDto } from "../types/PeopleCreateFullDto";
 
 export const getPersonNameByDni = async (dni: string): Promise<MinimalPeopleResponseDto> => {
   const response = await api.get<MinimalPeopleResponseDto>(`/people/getPersonName/${dni}`);
   return response.data;
+};
+
+export const createPerson = async (data: PeopleCreateFullDto): Promise<void> => {
+  await api.post("/people", data);
 };
