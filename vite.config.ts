1c502e4cf901334f0a8442e23445f3eed6c70915
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-19 22:47:29 -0500

    Refactor: reorganizar configuración de Vite y agregar opciones de chunking manual para optimización de dependencias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +8,25 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          nextui: [
+            "@nextui-org/react",
+            "@nextui-org/use-infinite-scroll",
+            "@nextui-org/use-disclosure",
+          ],
+          tanstack: ["@tanstack/react-query", "@tanstack/react-query-persist-client"],
+          zod: ["zod"],
+          axios: ["axios"],
+          framer: ["framer-motion"],
+          zustand: ["zustand"],
+        },
+      },
     },
   },
 });
