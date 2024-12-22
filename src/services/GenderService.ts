a4f2316ab9b8ab3435b7282dca93b3d1c3e4b283
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-22 00:29:01 -0500

    refactor: unificar el estilo de comillas en GenderService.ts

--- src/services/GenderService.ts
+++ src/services/GenderService.ts
@@ -1,5 +1,5 @@
-import { Gender } from "@/types/Gender";
-import { api } from "./api/api";
+import { Gender } from '@/types/Gender';
+import { api } from './api/api';
 
 export const getGender = async (): Promise<Gender[]> => {
   const response = await api.get<Gender[]>(`/sexType`);
