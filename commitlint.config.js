b7619ef783ab5df5d9f2773aec8187e65d723425
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-22 00:30:55 -0500

    feat: agregar configuración de Husky y Commitlint para mejorar el flujo de trabajo de commits

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+export const commitlintExtends = ['@commitlint/config-conventional'];
+export const rules = {
+  'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+  'type-case': [2, 'always', 'lower-case'],
+  'subject-case': [0],
+  'subject-empty': [0]
+};
