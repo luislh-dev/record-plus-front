cb70dced06233a08cbfece9925a6eef128b8e3af
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-22 23:00:27 -0500

    feat: agregar componente AlertPreRegister para guiar el registro de usuarios

---
+++ src/pages/user/components/AlertPreRegister.tsx
@@ -0,0 +1,12 @@
+import { Alert } from '@nextui-org/react';
+
+export const AlertPreRegister = () => {
+  return (
+    <Alert
+      color="warning"
+      variant="bordered"
+      title="Consejo"
+      description="Para registrar un nuevo usuario, primero ingrese el documento de identidad para cargar los datos personales. Luego complete la información de acceso al sistema"
+    />
+  );
+};
