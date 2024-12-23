56d62d157f6f2b1dd86886e29bab5e2d9af969e4
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-22 23:01:36 -0500

    feat: implementar formulario de creación de doctor con búsqueda de persona y validación de datos

--- src/pages/user/components/DoctorFrom.tsx
+++ src/pages/user/components/DoctorFrom.tsx
@@ -1,13 +1,26 @@
+import { CustomInput } from '@/components/CustomInput';
+import { CustomSelect } from '@/components/CustomSelect';
 import { Typography } from '@/components/Typography';
 import { useStates } from '@/hooks/state/useState';
 import { PeopleCreateModal } from '@/pages/people/components/PeopleCreateModal';
+import { PersonSearch } from '@/pages/people/components/PersonSearch';
 import { MinimalPeopleResponseDto } from '@/pages/people/types/MinimalPeopleResponseDto';
+import { PeopleCreateRequiredDto } from '@/pages/people/types/PeopleCreateRequiredDto';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Card, CardBody, CardHeader, useDisclosure } from '@nextui-org/react';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Divider,
+  Input,
+  useDisclosure
+} from '@nextui-org/react';
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useUserDoctorCreate } from '../hooks/useUserCreate';
 import { UserDoctorCreateValues, userDoctorCreateSchema } from '../models/userDoctorCreateSchema';
+import { AlertPreRegister } from './AlertPreRegister';
 
 export const DoctorForm = () => {
   const { isOpen, onClose, onOpen } = useDisclosure();
@@ -30,6 +43,7 @@ export const DoctorForm = () => {
     handleSubmit,
     setValue,
     watch,
+    reset,
     formState: { errors }
   } = useForm<UserDoctorCreateValues>({
     resolver: zodResolver(userDoctorCreateSchema),
@@ -52,12 +66,48 @@ export const DoctorForm = () => {
     handleCreate(data);
   };
 
+  const foundPerson = (person: MinimalPeopleResponseDto | null) => {
+    // Si no se encuentra a la persona, se limpian los campos
+    if (person === null && watch('personDNI')) {
+      // Solo hace reset si había datos previos
+      reset({
+        personDNI: '',
+        personalInfo: {
+          name: '',
+          surnames: '',
+          phone: ''
+        }
+      });
+      return;
+    }
+
+    if (person === null) return;
+
+    if (person.hasExternalSource) {
+      setPersonData(person);
+      onOpen();
+    } else {
+      setValue('personDNI', person.documentNumber);
+      setValue('personalInfo.name', person.name);
+      setValue('personalInfo.surnames', `${person.fatherLastName} ${person.motherLastName}`);
+      setValue('personalInfo.phone', person.phone);
+    }
+  };
+
+  const handleConfirmPerson = (data: PeopleCreateRequiredDto) => {
+    setValue('personDNI', data.documentNumber);
+    setValue('personalInfo.name', data.name);
+    setValue('personalInfo.surnames', `${data.paternalSurname} ${data.maternalSurname}`);
+    setValue('personalInfo.phone', data.phone);
+    onClose();
+  };
+
   return (
     <>
       <PeopleCreateModal
         isOpen={isOpen}
         onClose={onClose}
-        onConfirm={() => {}}
+        onConfirm={handleConfirmPerson}
         personData={personData || ({} as MinimalPeopleResponseDto)}
       />
       <section>
@@ -69,7 +119,110 @@ export const DoctorForm = () => {
               </Typography>
             </header>
           </CardHeader>
-          <CardBody></CardBody>
+          <CardBody>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+              {/* Buscador de persona */}
+              <div className="flex flex-col">
+                <div className="flex gap-4 mb-4">
+                  <div className="flex flex-col gap-2 w-full">
+                    <PersonSearch onPersonFound={foundPerson} />
+                    {errors.personDNI && (
+                      <Typography as="p" variant="error" className="h-4 pl-1">
+                        {errors.personDNI.message}
+                      </Typography>
+                    )}
+                  </div>
+                </div>
+                <AlertPreRegister />
+                {/* Información de la persona encontrada */}
+                <div className="flex flex-col mt-3">
+                  <Typography as="h5" variant="body-small" className="font-medium">
+                    Datos Encontrados
+                  </Typography>
+                  <div className="grid grid-cols-3 gap-4">
+                    <Input
+                      type="text"
+                      label="Nombre"
+                      value={watch('personalInfo.name') || ''}
+                      isDisabled
+                    />
+
+                    <Input
+                      type="text"
+                      label="Apellidos"
+                      value={watch('personalInfo.surnames') || ''}
+                      isDisabled
+                    />
+                    <Input
+                      type="phone"
+                      label="Teléfono"
+                      value={watch('personalInfo.phone') || ''}
+                      isDisabled
+                    />
+                  </div>
+                </div>
+              </div>
+              <Divider />
+              {/* Sección para al creación del usuario */}
+              <div className="flex flex-col gap-3">
+                <Typography as="h5" variant="body-small" className="font-medium">
+                  Datos de acceso
+                </Typography>
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="col-span-2">
+                    <CustomInput
+                      name="email"
+                      control={control}
+                      label="Email"
+                      error={errors.email}
+                      placeholder="Ingrese el email"
+                      isRequired
+                    />
+                  </div>
+
+                  <CustomInput
+                    name="name"
+                    control={control}
+                    label="Usuario"
+                    placeholder="Ingrese el nombre del usuario"
+                    isRequired
+                    error={errors.name}
+                  />
+                  <CustomSelect
+                    name="stateId"
+                    control={control}
+                    label="Estado"
+                    options={state}
+                    error={errors.stateId}
+                    isRequired
+                  />
+                  <CustomInput
+                    name="password"
+                    type="password"
+                    control={control}
+                    label="Contraseña"
+                    placeholder="Ingrese la contraseña"
+                    isRequired
+                    error={errors.password}
+                  />
+                  <CustomInput
+                    name="passwordConfirmation"
+                    type="password"
+                    control={control}
+                    label="Confirmar contraseña"
+                    placeholder="Confirme la contraseña"
+                    isRequired
+                    error={errors.passwordConfirmation}
+                  />
+                </div>
+              </div>
+              <div className="col-span-2 flex justify-end gap-2">
+                <Button type="submit" color="primary" isLoading={isSubmitting}>
+                  Guardar
+                </Button>
+              </div>
+            </form>
+          </CardBody>
         </Card>
       </section>
     </>
--- src/pages/user/hooks/useUserCreate.ts
+++ src/pages/user/hooks/useUserCreate.ts
@@ -1,8 +1,10 @@
 import { ApiServiceError } from '@/services/api/ApiErrorHandler';
 import { useState } from 'react';
+import { toDoctorCreationDto } from '../adapter/toDoctorCreationDto';
 import { toManagementCreationDto } from '../adapter/toManagementCreationDto';
+import { UserDoctorCreateValues } from '../models/userDoctorCreateSchema';
 import { UserManagementCreateValues } from '../models/userManagementCreateSchema';
-import { createManagementUser } from '../service/userService';
+import { createDoctorUser, createManagementUser } from '../service/userService';
 
 export function useUserManagementCreate() {
   const [isLoading, setLoading] = useState(false);
@@ -13,6 +15,30 @@ export function useUserManagementCreate() {
     setError(null);
     try {
       await createManagementUser(toManagementCreationDto(data));
+    } catch (error) {
+      if (error instanceof ApiServiceError) {
+        setError(error);
+      }
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const isSuccess = !isLoading && !error;
+
+  return { isLoading, error, handleCreate, isSuccess };
+}
+
+export function useUserDoctorCreate() {
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState<ApiServiceError | null>(null);
+
+  const handleCreate = async (data: UserDoctorCreateValues) => {
+    setLoading(true);
+    setError(null);
+    try {
+      await createDoctorUser(toDoctorCreationDto(data));
     } catch (error) {
       if (error instanceof ApiServiceError) {
         setError(error);
