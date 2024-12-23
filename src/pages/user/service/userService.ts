71333977b4483895d83b872370c94ecf7b591509
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-22 20:48:31 -0500

    feat(userService): agregar función para crear usuarios doctores

--- src/pages/user/service/userService.ts
+++ src/pages/user/service/userService.ts
@@ -1,5 +1,6 @@
 import { api } from '@/services/api/api';
 import { PageResponse } from '@/types/Pagination';
+import { DoctorCreationDto } from '../types/DoctorCreationDto';
 import { ManagementCreationDto } from '../types/ManagementCreationDto';
 import { UserListDTO } from '../types/UserListDTO';
 import { UserRequestParams } from '../types/UserRequestParams';
@@ -17,5 +18,10 @@ export const deleteUser = async (id: string): Promise<void> => {
 
 export const createManagementUser = async (data: ManagementCreationDto) => {
   const response = await api.post<ManagementCreationDto>('/users/createManagementUser', data);
+  return response.data;
+};
+
+export const createDoctorUser = async (data: DoctorCreationDto) => {
+  const response = await api.post<DoctorCreationDto>('/users/createDoctorUser', data);
   return response.data;
 };
