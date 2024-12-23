968da388710aa4a530d416b8ecd82971f2b7e9d2
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-22 23:01:42 -0500

    feat: agregar lógica para renderizar formularios según roles de usuario

--- src/pages/user/UserAdd.tsx
+++ src/pages/user/UserAdd.tsx
@@ -1,5 +1,18 @@
+import { useIsAdmin, useIsManagement } from '@/hooks/auth/useAuthRoles';
+import { DoctorForm } from './components/DoctorFrom';
 import { ManagementForm } from './components/ManagementForm';
 
 export const UserAdd = () => {
-  return <ManagementForm />;
+  const isAdmin = useIsAdmin();
+  const isManagement = useIsManagement();
+
+  if (isAdmin) {
+    return <ManagementForm />;
+  }
+
+  if (isManagement) {
+    return <DoctorForm />;
+  }
+
+  return null;
 };
