789fbe6b3f5fc3425a8e5d3c5b82935dea6aa88e
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-25 14:58:32 -0500

    feat: agregar componente DropDownFilter y constantes de parámetros de búsqueda

---
+++ src/pages/people/components/DropDownFilter.tsx
@@ -0,0 +1,49 @@
+import { Tune } from '@/icons/Tune';
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+  Radio,
+  RadioGroup
+} from '@nextui-org/react';
+import { SEARCH_PARAMS } from '../constants/SearchParams';
+import { usePersonSearch } from '../hooks/usePersonSearch';
+import { useSearchPeopleStore } from '../stores/useSearchStore';
+import { SearchFieldKeys } from '../types/PeopleRequestParams';
+
+export const DropDownFilter = () => {
+  const { searchTerm, selectedSearchField, setSelectedSearchField } = useSearchPeopleStore();
+  const { refetch } = usePersonSearch();
+  const handleSearchParamChange = (selectedValue: string) => {
+    setSelectedSearchField(selectedValue as SearchFieldKeys);
+    if (searchTerm !== '') refetch();
+  };
+
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Button startContent={<Tune />}>Filtrar</Button>
+      </DropdownTrigger>
+      <DropdownMenu variant="faded" aria-label="Lista de filtros">
+        <DropdownSection title="Filtros de busqueda" aria-label="Filtros de busqueda">
+          <DropdownItem textValue="Parámetro de busqueda" key={'peopleParams'}>
+            <RadioGroup
+              aria-label="Seleccionar parámetro de busqueda"
+              value={selectedSearchField}
+              onValueChange={handleSearchParamChange}
+            >
+              {SEARCH_PARAMS.map(param => (
+                <Radio key={param.id} value={param.id}>
+                  {param.label}
+                </Radio>
+              ))}
+            </RadioGroup>
+          </DropdownItem>
+        </DropdownSection>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
---
+++ src/pages/people/constants/SearchParams.ts
@@ -0,0 +1,11 @@
+import { SearchFieldKeys } from '../types/PeopleRequestParams';
+
+interface SearchParam {
+  id: SearchFieldKeys;
+  label: string;
+}
+
+export const SEARCH_PARAMS: SearchParam[] = [
+  { id: 'documentNumber', label: 'Número de documento' },
+  { id: 'fullName', label: 'Nombre' }
+];
