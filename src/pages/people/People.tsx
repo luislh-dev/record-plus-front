24cad16201121115823faa7bfde11ae91f22576d
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-25 15:07:26 -0500

    feat: agregar componente HeaderList para mostrar el encabezado de la lista de pacientes

--- src/pages/people/People.tsx
+++ src/pages/people/People.tsx
@@ -1,10 +1,12 @@
 import { DropDownFilter } from './components/DropDownFilter';
+import { HeaderList } from './components/HeaderList';
 import { PeopleList } from './components/PeopleList';
 import { Search } from './components/Search';
 
 const People = () => {
   return (
     <section>
+      <HeaderList />
       <nav className="flex gap-x-4 px-2 pb-2 pt-4">
         <Search />
         <DropDownFilter />
---
+++ src/pages/people/components/HeaderList.tsx
@@ -0,0 +1,19 @@
+import { Add } from '@/icons/Add';
+import { Button } from '@nextui-org/react';
+import { useNavigate } from 'react-router-dom';
+
+export const HeaderList = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="flex max-w-full justify-between px-3">
+      <h1 className="text-2xl font-bold">Lista de Pacientes</h1>
+      <Button
+        color="primary"
+        onPress={() => navigate('/people/add')}
+        endContent={<Add size={18} />}
+      >
+        Registrar paciente
+      </Button>
+    </div>
+  );
+};
