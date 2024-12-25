b5021aaeb9e6320cb1702c979702e7528331deff
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-25 11:37:16 -0500

    feat: agregar React Query Devtools para mejorar la depuración de consultas

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
+
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { queryClient } from './config/queryClient';
@@ -16,6 +18,7 @@ root.render(
       <AuthProvider>
         <RouterProvider router={router} />
       </AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom" />
     </QueryClientProvider>
   </React.StrictMode>
 );
