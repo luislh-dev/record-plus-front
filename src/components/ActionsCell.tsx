bc862ffdbeb97dd7d687602968f13c57382e0f10
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-25 23:02:38 -0500

    feat: agregar funcionalidad de detalle en el componente ActionsCell

--- src/components/ActionsCell.tsx
+++ src/components/ActionsCell.tsx
@@ -1,39 +1,61 @@
-import { Tooltip } from '@nextui-org/react';
-import { EditIcon } from '@/icons/EditIcon';
 import { DeleteIcon } from '@/icons/DeleteIcon';
+import { EditIcon } from '@/icons/EditIcon';
+import { EyeIcon } from '@/icons/EyeIcon';
+import { Tooltip } from '@nextui-org/react';
 
 interface ActionsCellProps {
-  state: string;
-  onEdit: () => void;
-  onDelete: () => void;
-  inactiveStates: string[];
+  state?: string;
+  onEdit?: () => void;
+  onDelete?: () => void;
+  onDetail?: () => void;
+  inactiveStates?: string[];
 }
 
-export function ActionsCell({ state, onEdit, onDelete, inactiveStates }: ActionsCellProps) {
-  const isInactive = inactiveStates.includes(state);
+export function ActionsCell({
+  state,
+  onEdit,
+  onDelete,
+  inactiveStates,
+  onDetail
+}: ActionsCellProps) {
+  const isInactive = state && inactiveStates?.includes(state);
 
   return (
     <div className="relative flex items-center justify-center w-full gap-2">
-      <Tooltip content="Editar">
-        <span
-          className="text-lg text-default-400 cursor-pointer active:opacity-50"
-          onClick={onEdit}
-        >
-          <EditIcon />
-        </span>
-      </Tooltip>
-      <Tooltip content="Eliminar">
-        <span
-          className={`text-lg transition-all duration-200 ${
-            isInactive
-              ? 'text-danger/40 pointer-events-none cursor-not-allowed opacity-70'
-              : 'text-danger cursor-pointer active:opacity-50 hover:opacity-80'
-          }`}
-          onClick={isInactive ? undefined : onDelete}
-        >
-          <DeleteIcon />
-        </span>
-      </Tooltip>
+      {onEdit && (
+        <Tooltip content="Editar">
+          <span
+            className="text-lg text-default-400 cursor-pointer active:opacity-50"
+            onClick={onEdit}
+          >
+            <EditIcon />
+          </span>
+        </Tooltip>
+      )}
+      {onDelete && (
+        <Tooltip content="Eliminar">
+          <span
+            className={`text-lg transition-all duration-200 ${
+              isInactive
+                ? 'text-danger/40 pointer-events-none cursor-not-allowed opacity-70'
+                : 'text-danger cursor-pointer active:opacity-50 hover:opacity-80'
+            }`}
+            onClick={isInactive ? undefined : onDelete}
+          >
+            <DeleteIcon />
+          </span>
+        </Tooltip>
+      )}
+      {onDetail && (
+        <Tooltip content="Detalle">
+          <span
+            className="text-lg text-default-400 cursor-pointer active:opacity-50"
+            onClick={onDetail}
+          >
+            <EyeIcon />
+          </span>
+        </Tooltip>
+      )}
     </div>
   );
 }
