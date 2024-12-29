6faed6c22caaf2da20916c74a0da9a98a21a6d95
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-28 19:31:01 -0500

    feat: agregar componente HeaderRecord para mostrar el título y botón de nuevo registro en el historial médico

---
+++ src/pages/people/components/HeaderRecord.tsx
@@ -0,0 +1,23 @@
+import { Typography } from '@/components/Typography';
+import { Add } from '@/icons/Add';
+import { Button } from '@nextui-org/react';
+import { useNavigate } from 'react-router-dom';
+
+export const HeaderRecord = () => {
+  const navigate = useNavigate();
+  return (
+    <header className="flex justify-between items-center w-full">
+      <Typography as="h2" variant="section-title">
+        Historial Medico
+      </Typography>
+      <Button
+        color="primary"
+        variant="solid"
+        endContent={<Add size={20} />}
+        onPress={() => navigate(`record/add`)}
+      >
+        Nuevo Registro
+      </Button>
+    </header>
+  );
+};
