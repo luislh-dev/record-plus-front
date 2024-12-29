408341619a24221ba06b141f68b03ca4ac8781c0
luislopez-developer <luislopezhuari27@gmail.com>
2024-12-28 19:22:26 -0500

    feat: agregar hook y servicio para obtener tipos de archivo

---
+++ src/hooks/fileType/useFileType.ts
@@ -0,0 +1,14 @@
+import { ONE_MONTH_IN_MS } from '@/constants/times';
+import { getAllFileTypes } from '@/services/FileTypeService';
+import { useQuery } from '@tanstack/react-query';
+
+export const useFileType = () => {
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ['fileType'],
+    queryFn: async () => getAllFileTypes(),
+    staleTime: ONE_MONTH_IN_MS,
+    gcTime: ONE_MONTH_IN_MS
+  });
+
+  return { fileTypes: data ?? [], isError, isLoading };
+};
---
+++ src/services/FileTypeService.ts
@@ -0,0 +1,7 @@
+import { FileType } from '@/types/FileType';
+import { api } from './api/api';
+
+export const getAllFileTypes = async (): Promise<FileType[]> => {
+  const response = await api.get<FileType[]>(`/file-type`);
+  return response.data;
+};
