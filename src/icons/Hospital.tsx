7992bba4a09d4f71812bdafd7485441dc88fea31
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-06 08:45:14 -0500

    fix: corrige atributos SVG usando convención camelCase

--- src/icons/Hospital.tsx
+++ src/icons/Hospital.tsx
@@ -8,9 +8,9 @@ export const Hospital = (props: iconProps) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     {...props}
   >
     <path d="M12 6v4" />
