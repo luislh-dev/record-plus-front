6dba9a03601b30b18168cdb8ff12272da90a6097
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-05 23:08:43 -0500

    feat: agregar mocks para useFileType y useStates en pruebas

---
+++ src/test/__mocks__/hooks/mockFileType.ts
@@ -0,0 +1,10 @@
+export const mockFileType = {
+  useFileType: () => ({
+    fileTypes: [
+      { id: 1, name: 'Documento' },
+      { id: 2, name: 'Imagen' }
+    ],
+    isLoading: false,
+    isError: false
+  })
+};
---
+++ src/test/__mocks__/hooks/mockState.ts
@@ -0,0 +1,10 @@
+export const mockState = {
+  useStates: () => ({
+    state: [
+      { id: 1, name: 'Activo' },
+      { id: 2, name: 'Inactivo' }
+    ],
+    isLoading: false,
+    isError: false
+  })
+};
