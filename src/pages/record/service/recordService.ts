d969774d16a45efb961c3e5d3c1904c627f49706
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-07 16:26:33 -0500

    feat: agregar función para obtener la lista de detalles de registro por ID de persona

--- src/pages/record/service/recordService.ts
+++ src/pages/record/service/recordService.ts
@@ -1,4 +1,7 @@
 import { api } from '@/services/api/api';
+import { PageResponse } from '@/types/Pagination';
+import { RecordDetailListResponseDto } from '../types/RecordDetailListResponseDto';
+import { RecordDetailRequestParams } from '../types/RecordDetailRequestParams';
 
 export const createRecordDetail = async (data: FormData) => {
   const response = await api.post('/record-details', data, {
@@ -6,6 +9,20 @@ export const createRecordDetail = async (data: FormData) => {
       'Content-Type': 'multipart/form-data'
     }
   });
+
+  return response.data;
+};
+
+export const getRecordDetailList = async (
+  peopleId: string,
+  params?: RecordDetailRequestParams
+): Promise<PageResponse<RecordDetailListResponseDto>> => {
+  const response = await api.get<PageResponse<RecordDetailListResponseDto>>(
+    `/record-details/findAllByPeopleId/${peopleId}`,
+    {
+      params
+    }
+  );
 
   return response.data;
 };
