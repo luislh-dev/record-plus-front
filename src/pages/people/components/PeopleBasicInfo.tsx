ddabb7784e6f3310fdcd89172f398ce0fba13db1
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-07 16:24:51 -0500

    feat: mejorar la presentación de información básica de la persona con un nuevo diseño de tarjeta

--- src/pages/people/components/PeopleBasicInfo.tsx
+++ src/pages/people/components/PeopleBasicInfo.tsx
@@ -1,5 +1,6 @@
-import { Typography } from '@/components/Typography';
+import { Person } from '@/icons/Person';
 import { calculateAge } from '@/utils/peruDateTime';
+import { Card, CardBody } from '@nextui-org/react';
 import { useEffect } from 'react';
 import { useGetPersonDetailById } from '../hooks/useGetPerson';
 
@@ -17,10 +18,55 @@ export const PeopleBasicInfo = ({ personId }: Props) => {
   const personAge = data?.birthdate ? calculateAge(data.birthdate) : null;
 
   return (
-    <Typography variant="label">
-      {`${data?.name} ${data?.paternalSurname} ${data?.maternalSurname}`} -{' '}
-      {data?.typeDocument.name}: {data?.documentNumber}{' '}
-      {personAge !== null && ` - ${personAge} años`}
-    </Typography>
+    <Card className="bg-white shadow-lg">
+      <CardBody className="p-6">
+        <div className="space-y-4">
+          {/* Nombre */}
+          <div className="flex items-center space-x-3 pb-4 border-b">
+            <Person size={20} fill="black" />
+            <div>
+              <div className="text-sm font-semibold text-gray-800">
+                {data?.name} {data?.paternalSurname} {data?.maternalSurname}
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {/* Tipo de documento */}
+            <div className="flex items-center space-x-3">
+              <div>
+                <div className="text-sm text-gray-500">{data?.typeDocument.name}</div>
+                <div className="font-medium">{data?.documentNumber}</div>
+              </div>
+            </div>
+
+            {/* Nacionalidad */}
+            <div>
+              <div className="text-sm text-gray-500">Nacionalidad</div>
+              <div className="font-medium">{data?.nationality || '-'}</div>
+            </div>
+
+            {/* Fecha de nacimiento */}
+            <div>
+              <div className="text-sm text-gray-500">Nacimiento</div>
+              <div className="font-medium">{data?.birthdate || '-'}</div>
+            </div>
+
+            {/* Edad */}
+            <div>
+              <div className="text-sm text-gray-500">Edad</div>
+              <div className="font-medium">{personAge !== null ? `${personAge} años` : '-'}</div>
+            </div>
+
+            {/* genero */}
+            <div className="flex items-center space-x-3">
+              <div>
+                <div className="text-sm text-gray-500">Género</div>
+                <div className="font-medium">{data?.sexType.name}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </CardBody>
+    </Card>
   );
 };
