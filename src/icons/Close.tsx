84ac412f288b955e032c0a783b74748940712698
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-08 00:23:12 -0500

    feat: agregar componente Close como ícono SVG reutilizable

---
+++ src/icons/Close.tsx
@@ -0,0 +1,15 @@
+import { iconProps } from './iconProps';
+
+export const Close = (props: iconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height={props.size || props.height || '24px'}
+    viewBox="0 -960 960 960"
+    width={props.size || props.width || '24px'}
+    fill={props.fill || '#e8eaed'}
+    stroke="currentColor"
+    {...props}
+  >
+    <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+  </svg>
+);
