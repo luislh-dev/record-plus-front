419e7762d4196fed9abcdf6ac77fabfe02832eda
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-07 23:50:38 -0500

    feat: agregar hook useRecordDetailSearch y store useSearchStoreRecordDetail para búsqueda de detalles de registro

---
+++ src/pages/record/hooks/useRecordDetailSearch.tsx
@@ -0,0 +1,46 @@
+import { useDebounce } from '@/hooks/useDebounce';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { getRecordDetailList } from '../service/recordService';
+import { useSearchStoreRecordDetail } from '../store/useSearchStoreRecordDetail';
+
+export function useRecordDetailSearch() {
+  const { buildSearchParams, setRangeDate, searchTerm, setSearchTerm, rangeDate } =
+    useSearchStoreRecordDetail();
+  const [personId, setPersonId] = useState<string | null>(null);
+  const debouncedSearchTerm = useDebounce(searchTerm, 300);
+
+  const fetchRecordDetails = async ({ pageParam = 0 }) => {
+    if (!personId) throw new Error('ID no proporcionado');
+    return getRecordDetailList(personId, {
+      ...buildSearchParams(),
+      page: pageParam
+    });
+  };
+
+  const { data, isLoading, isError, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ['recordDetail', personId, debouncedSearchTerm, rangeDate],
+    queryFn: fetchRecordDetails,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => (lastPage.last ? undefined : allPages.length),
+    enabled: !!personId
+  });
+
+  const recordDetails = data?.pages.flatMap(page => page.content) ?? [];
+  const totalElements = data?.pages[0].totalElements ?? 0;
+  const currentPage = data?.pages[0].number ?? 0;
+
+  return {
+    recordDetails,
+    isLoading,
+    isError,
+    totalElements,
+    currentPage,
+    fetchNextPage,
+    hasNextPage,
+    setPersonId,
+    setRangeDate,
+    searchTerm,
+    setSearchTerm
+  };
+}
---
+++ src/pages/record/store/useSearchStoreRecordDetail.ts
@@ -0,0 +1,41 @@
+import { dateUtils } from '@/utils/dateUtils';
+import { CalendarDate } from '@internationalized/date';
+import { create } from 'zustand';
+import { RecordDetailRequestParams } from '../types/RecordDetailRequestParams';
+
+interface SearchState {
+  searchTerm: string;
+  rangeDate: readonly [CalendarDate | null, CalendarDate | null];
+  size: number;
+
+  setRangeDate: (rangeDate: readonly [CalendarDate | null, CalendarDate | null]) => void;
+  setSearchTerm: (searchTerm: string) => void;
+  buildSearchParams: () => Omit<RecordDetailRequestParams, 'page'>;
+}
+export const useSearchStoreRecordDetail = create<SearchState>((set, get) => ({
+  searchTerm: '',
+  size: 4,
+  rangeDate: [null, null],
+
+  setRangeDate: rangeDate => set({ rangeDate }),
+  setSearchTerm: term => set({ searchTerm: term }),
+
+  buildSearchParams: () => {
+    const { searchTerm, size, rangeDate } = get();
+
+    const params: Omit<RecordDetailRequestParams, 'page'> = {
+      size
+    };
+
+    if (searchTerm) {
+      params.hospitalName = searchTerm;
+    }
+
+    if (rangeDate[0] && rangeDate[1]) {
+      params.startDate = dateUtils.formatDateForAPI(rangeDate[0]);
+      params.endDate = dateUtils.formatDateForAPI(rangeDate[1], true);
+    }
+
+    return params;
+  }
+}));
