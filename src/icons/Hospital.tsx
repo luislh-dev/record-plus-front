f0f41a7b44dd78ceb7975068dec55bc854267b7b
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-09 19:28:50 -0500

    feat: agregar nuevos íconos: Calendar, Paperclip, Pill y Stethoscope; actualizar color de trazo en Hospital

--- src/icons/Hospital.tsx
+++ src/icons/Hospital.tsx
@@ -6,8 +6,8 @@ export const Hospital = (props: iconProps) => (
     width={props.size || props.width || 24}
     height={props.size || props.height || 24}
     viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
+    fill={'none'}
+    stroke={props.color || '#e8eaed'}
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
