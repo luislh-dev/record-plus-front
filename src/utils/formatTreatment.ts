f9804426766259e224d1a5c15e05c1441f49ad95
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-09 19:28:21 -0500

    feat: agregar función formatTreatment para formatear cadenas de tratamiento

---
+++ src/utils/formatTreatment.ts
@@ -0,0 +1,6 @@
+export const formatTreatment = (treatment: string) => {
+  return treatment
+    ?.replace(/\\n/g, '\n') // Reemplaza \\n por \n
+    .split('\n') // Divide en líneas
+    .map(line => line.replace(/^\d+\.\s*/, '')); // Elimina los números del inicio
+};
