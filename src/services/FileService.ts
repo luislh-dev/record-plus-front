77f4164608c037cf4f95679efdf31a988bd9d71b
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-16 18:51:23 -0500

    feat: agregar hook usePresignedUrl para obtener URLs prefirmadas de archivos

---
+++ src/services/FileService.ts
@@ -0,0 +1,10 @@
+import { api } from './api/api';
+
+export const getPresignedUrlByObjectKey = async (objectKey: string): Promise<string | null> => {
+  const response = await api.get<string>(`/r2/presigned-url`, {
+    params: {
+      objectKey
+    }
+  });
+  return response.data;
+};
