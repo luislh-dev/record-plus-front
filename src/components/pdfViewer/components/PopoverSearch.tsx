d007bb5060c53de56b241925f554d715e8339886
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-16 19:23:27 -0500

    feat: agregar componente PopoverSearch para mejorar la funcionalidad de búsqueda en el visor de PDF

---
+++ src/components/pdfViewer/components/PopoverSearch.tsx
@@ -0,0 +1,23 @@
+import { Popover } from '@/components/Popover/components/Popover';
+import { PopoverContent } from '@/components/Popover/components/PopoverContent';
+import { PopoverTrigger } from '@/components/Popover/components/PopoverTrigger';
+import { Search } from '@/icons/Search';
+import { ControlButton } from './ControlButton';
+import { PDFSearchControls } from './PDFSearchControls';
+
+export const PropoverSearch = () => {
+  return (
+    <Popover isDismissable={false} placement="bottom-start">
+      <PopoverTrigger>
+        <ControlButton>
+          <Search size={20} />
+        </ControlButton>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="p-2">
+          <PDFSearchControls />
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
