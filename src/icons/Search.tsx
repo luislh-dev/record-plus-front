370294a49a2c81bf08d1885ebf5b43939294a989
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-16 19:22:59 -0500

    feat: actualizar el ícono de búsqueda para mejorar la visualización y la accesibilidad

--- src/icons/Search.tsx
+++ src/icons/Search.tsx
@@ -4,14 +4,18 @@ export function Search(props: iconProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      height={props.size || props.height || 20}
-      viewBox="0 -960 960 960"
-      width={props.size || props.width || 20}
-      fill={props.fill || '#64748b'}
-      strokeWidth={props.strokeWidth || 2}
+      width={props.size || props.height || '24'}
+      height={props.size || props.height || '24'}
+      viewBox="0 0 24 24"
+      fill={props.fill || 'none'}
+      stroke={props.stroke || 'currentColor'}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
-      <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+      <circle cx="11" cy="11" r="8" />
+      <path d="m21 21-4.3-4.3" />
     </svg>
   );
 }
