9d9f07abe681a5c5e9dafea958e6d473af700992
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-25 08:41:11 -0500

    feat(tailwind): actualizar configuraciones de Tailwind para incluir Zeit UI y mejorar la consistencia de comillas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-import { nextui } from "@nextui-org/react";
+import { nextui } from '@nextui-org/react';
 
 /** @type {import('tailwindcss').Config} */
 export const content = [
-  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  "./src/**/*.{js,ts,jsx,tsx}",
+  './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  './node_modules/@zeitui-org/**/*.{js,jsx,ts,tsx}',
+  './src/**/*.{js,ts,jsx,tsx}'
 ];
 export const theme = {
-  extend: {},
+  extend: {}
 };
-export const darkMode = "class";
+export const darkMode = 'class';
 export const plugins = [nextui()];
