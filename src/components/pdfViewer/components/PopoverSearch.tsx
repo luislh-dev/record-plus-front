7e1609df16b367045773c182ed28ff3a888103d1
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-31 19:50:58 -0500

    feat(PopoverSearch): corregir estilo de comillas en el componente PopoverSearch

--- src/components/pdfViewer/components/PopoverSearch.tsx
+++ src/components/pdfViewer/components/PopoverSearch.tsx
@@ -2,19 +2,19 @@ import { Popover } from '@/components/Popover/components/Popover';
 import { PopoverContent } from '@/components/Popover/components/PopoverContent';
 import { PopoverTrigger } from '@/components/Popover/components/PopoverTrigger';
 import { Search } from '@/icons/Search';
-import { ControlButton } from './common/ControlButton';
 import { PDFSearchControls } from './PDFSearchControls';
+import { ControlButton } from './common/ControlButton';
 
 export const PropoverSearch = () => {
   return (
-    <Popover isDismissable={false} placement="bottom-start">
+    <Popover isDismissable={false} placement='bottom-start'>
       <PopoverTrigger>
         <ControlButton>
           <Search size={20} />
         </ControlButton>
       </PopoverTrigger>
       <PopoverContent>
-        <div className="p-2">
+        <div className='p-2'>
           <PDFSearchControls />
         </div>
       </PopoverContent>
