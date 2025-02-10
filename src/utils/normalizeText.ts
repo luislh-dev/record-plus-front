864873efbe080b98369d6c9a43876c88915634cd
luislopez-developer <luislopezhuari27@gmail.com>
2025-02-10 00:52:06 -0500

    fix(normalizeText): eliminar función de normalización de texto no utilizada

--- src/utils/normalizeText.ts
+++
@@ -1,6 +0,0 @@
-export const normalizeText = (text: string): string => {
-  return text
-    .normalize('NFD') // Descompone los caracteres acentuados
-    .replace(/[\u0300-\u036f]/g, '') // Elimina diacríticos
-    .toLowerCase();
-};
