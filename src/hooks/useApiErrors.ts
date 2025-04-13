86d0959178e323c35c4f020d4aa62f0aef40d77b
Luis López <luislopezhuari27@gmail.com>
2025-04-13 13:26:28 -0500

    fix(useApiErrors): optimizar el manejo de errores con useCallback para mejorar el rendimiento

--- src/hooks/useApiErrors.ts
+++ src/hooks/useApiErrors.ts
@@ -1,28 +1,27 @@
 import type { ApiError } from '@/types/errros/ApiError';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const useApiErrors = (apiErrors: ApiError | null | undefined) => {
   const [backendErrors, setBackendErrors] = useState<Record<string, string>>({});
 
-  const parseErrors = (details: string[]) => {
+  const parseErrors = useCallback((details: string[]) => {
     const parsed: Record<string, string> = {};
     for (const detail of details) {
       const [field, message] = detail.split(': ');
       parsed[field.toLowerCase()] = message;
     }
     return parsed;
-  };
+  }, []);
 
-  const resetErrors = () => setBackendErrors({});
+  const resetErrors = useCallback(() => setBackendErrors({}), []);
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
     if (apiErrors?.details) {
       setBackendErrors(parseErrors(apiErrors.details));
     } else {
       resetErrors();
     }
-  }, [apiErrors]);
+  }, [apiErrors, parseErrors, resetErrors]);
 
   return {
     backendErrors,
