ba02d1eb21c490a9e60891ef1545d13e813609b1
Luis López <luislopezhuari27@gmail.com>
2025-05-03 18:04:47 -0500

    feat(hospital): mejorar UI en edición con feedback de carga, manejo de errores y navegación clara

--- src/pages/hospital/HospitalEdit.tsx
+++ src/pages/hospital/HospitalEdit.tsx
@@ -1,4 +1,5 @@
 import { HeaderForm } from '@/components/form/HeaderForm';
+import { Button, Link, Spinner } from '@heroui/react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { HospitalForm } from './components/HospitalForm';
 import { useHospitalGetBy } from './hooks/useHospitalGetBy';
@@ -23,16 +24,33 @@ const HospitalEdit = () => {
   const onSubmit = async (data: HospitalCreateValues) => {
     if (!id) return;
     await handleUpdate(Number.parseInt(id), data);
-    navigate(-1);
+    handleGoBack();
+  };
+
+  const handleGoBack = () => {
+    navigate('/hospitals');
   };
 
   if (isLoadingHospital) {
-    return <div>Cargando...</div>;
+    return (
+      <Spinner className='flex items-center justify-center h-full w-full p-4' label='Cargando...' />
+    );
   }
 
   if (getError) {
-    return <div>Error: {getError}</div>;
+    return (
+      <div className='flex items-center justify-center h-full w-full p-4'>
+        <div className='text-center space-y-2'>
+          <h1 className='text-2xl font-bold text-red-600'>Error</h1>
+          <p className='text-gray-500'>{getError}</p>
+          <Button onPress={handleGoBack} as={Link}>
+            Volver
+          </Button>
+        </div>
+      </div>
+    );
   }
+
   return (
     <div className='flex flex-col gap-4 w-full md:w-[600px]'>
       <HeaderForm
