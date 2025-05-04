30cd90e754a8adf9fb2ff769d608fe69022617f6
Luis López <luislopezhuari27@gmail.com>
2025-05-04 11:37:49 -0500

    refactor(Header): se quito padding y se actualizo el evento del boton

--- src/pages/hospital/components/Header.tsx
+++ src/pages/hospital/components/Header.tsx
@@ -5,11 +5,11 @@ import { useNavigate } from 'react-router-dom';
 export function Header() {
   const navigate = useNavigate();
   return (
-    <div className='flex max-w-full justify-between px-3'>
+    <div className='flex max-w-full justify-between'>
       <h1 className='text-2xl font-bold'>Lista de Hospitales</h1>
       <Button
         color='primary'
-        onClick={() => navigate('/hospitals/add')}
+        onPress={() => navigate('/hospitals/add')}
         endContent={<Add size={18} />}
       >
         Agregar Hospital
