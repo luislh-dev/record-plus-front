a76c0223c66eb243c24c611afd52be0ed67e184d
Luis López <luislopezhuari27@gmail.com>
2025-06-29 15:58:50 -0500

    feat: hacer que el componente ErrorPanel sea más flexible al permitir propiedades opcionales para error, estado y mensaje

--- src/components/errors/ErrorPanel.tsx
+++ src/components/errors/ErrorPanel.tsx
@@ -3,18 +3,20 @@ import { Button, Link } from '@heroui/react';
 import { useNavigate } from 'react-router-dom';
 
 interface Props {
-  error: ApiError;
+  error?: ApiError;
   redirectTo?: string;
+  status?: number;
+  message?: string;
 }
 
-export const ErrorPanel = ({ error, redirectTo = '/' }: Props) => {
+export const ErrorPanel = ({ error, redirectTo = '/', status, message }: Props) => {
   const navigate = useNavigate();
 
   return (
     <div className='flex items-center justify-center h-full w-full p-4'>
       <div className='text-center space-y-4'>
-        <h1 className='text-6xl font-bold text-gray-900'>{error.status}</h1>
-        <p className='text-gray-500'>{error.message}</p>
+        <h1 className='text-6xl font-bold text-gray-900'>{error?.status || status || 404}</h1>
+        <p className='text-gray-500'>{error?.message || message || 'Error Desconocido'}</p>
         <Button onPress={() => navigate(redirectTo)} className='mt-4' as={Link}>
           Volver al inicio
         </Button>
