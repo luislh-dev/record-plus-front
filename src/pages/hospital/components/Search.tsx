13d988fd44d719d9dbf851073271727adaaf82f4
Luis López <luislopezhuari27@gmail.com>
2025-07-07 00:02:21 -0500

    fix(Search): ajustar la propiedad isClearable en el componente de búsqueda

--- src/pages/hospital/components/Search.tsx
+++ src/pages/hospital/components/Search.tsx
@@ -11,10 +11,10 @@ export const SearchImput = () => {
         base: 'max-w-full sm:max-w-[15rem] h-10',
         mainWrapper: 'h-full',
         input: 'text-small',
-        inputWrapper:
-          'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20',
+        inputWrapper: 'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20',
       }}
       onClear={() => setSearchTerm('')}
+      isClearable
       type='text'
       placeholder='Buscar hospital...'
       value={searchTerm}
