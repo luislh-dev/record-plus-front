172206495ae12fd18eb80f5aee0a5b834d243597
Luis López <luislopezhuari27@gmail.com>
2025-07-12 13:22:15 -0500

    feat(AllergyCreateModal): agregar modal para crear alergías con validación y manejo de errores

---
+++ src/pages/allergies/components/AllergyCreateModal.tsx
@@ -0,0 +1,117 @@
+import { CustomInput } from '@/components/CustomInput';
+import { TextAreaForm } from '@/components/TextAreaForm';
+import { AllergyCategoryController } from '@/components/controllers/AllergyCategoryController';
+import type { ApiError } from '@/types/errros/ApiError';
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  type UseDisclosureProps,
+  addToast,
+} from '@heroui/react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { useAllergyCreate } from '../hooks/useAllergyCreate';
+import { type AllergyCreateValues, allergyCreateSchema } from '../models/allergyCreateSchema';
+
+interface AllergyCreateModalProps extends UseDisclosureProps {
+  onSave?: (data: AllergyCreateValues) => void | Promise<void>;
+}
+
+export function AllergyCreateModal({ onSave, ...props }: AllergyCreateModalProps) {
+  const { handleCreate, state } = useAllergyCreate();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<AllergyCreateValues>({
+    mode: 'onTouched',
+    resolver: zodResolver(allergyCreateSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+      category: '',
+    },
+  });
+
+  const onSubmit = async (data: AllergyCreateValues) => {
+    try {
+      await handleCreate(data);
+
+      addToast({
+        title: 'Alergía creada',
+        description: 'La alergía ha sido creada exitosamente.',
+        severity: 'success',
+        color: 'success',
+        timeout: 5000,
+      });
+      reset();
+      props.onChange?.(true);
+    } catch (error) {
+      const typedError = error as ApiError;
+
+      const errorDescription =
+        typedError.details && typedError.details.length > 0
+          ? typedError.details.map((detail) => detail.message).join('\n')
+          : typedError.message || 'Ocurrió un error al crear la alergía.';
+
+      addToast({
+        title: 'Error al crear alergía',
+        description: errorDescription,
+        severity: 'danger',
+        color: 'danger',
+        timeout: 10000,
+      });
+    }
+  };
+
+  return (
+    <Drawer isOpen={props.isOpen} onOpenChange={props.onChange}>
+      <DrawerContent>
+        {(onClose) => (
+          <form className='flex flex-col h-full' onSubmit={handleSubmit(onSubmit)}>
+            <DrawerHeader>Formulario para crear una Alergía</DrawerHeader>
+            <DrawerBody>
+              <CustomInput
+                name='name'
+                control={control}
+                label='Nombre de la alergia'
+                placeholder='Ingrese el nombre de la alergia'
+                isRequired
+                error={errors.name}
+              />
+              <AllergyCategoryController
+                name='category'
+                control={control}
+                label='Categoría de alergia'
+                isRequired
+                error={errors.category}
+              />
+              <TextAreaForm
+                name='description'
+                control={control}
+                label='Descripción'
+                placeholder='Ingrese una descripción de la alergia'
+                error={errors.description}
+                isRequired={false}
+              />
+            </DrawerBody>
+            <DrawerFooter>
+              <Button color='danger' variant='flat' onPress={onClose}>
+                Cerrar
+              </Button>
+              <Button color='primary' type='submit' isLoading={state.isLoading}>
+                Guardar
+              </Button>
+            </DrawerFooter>
+          </form>
+        )}
+      </DrawerContent>
+    </Drawer>
+  );
+}
--- src/pages/allergies/components/Header.tsx
+++ src/pages/allergies/components/Header.tsx
@@ -1,20 +1,27 @@
 import { Add } from '@/icons/Add';
-import { Button, Chip } from '@heroui/react';
+import { Button, Chip, useDisclosure } from '@heroui/react';
+import { AllergyCreateModal } from './AllergyCreateModal';
 
 interface props {
   totalAllergies?: number;
 }
 
 export function Header({ totalAllergies }: props) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
-    <div className='flex max-w-full justify-between h-full items-center'>
-      <div className='flex gap-1 items-center'>
-        <h1 className='text-2xl font-bold'>Lista de Alergias</h1>
-        {totalAllergies !== undefined && <Chip className='font-bold text-inherit'>{totalAllergies}</Chip>}
+    <>
+      <div className='flex max-w-full justify-between h-full items-center'>
+        <div className='flex gap-1 items-center'>
+          <h1 className='text-2xl font-bold'>Lista de Alergias</h1>
+          {totalAllergies !== undefined && <Chip className='font-bold text-inherit'>{totalAllergies}</Chip>}
+        </div>
+        <Button color='primary' endContent={<Add size={18} />} onPress={onOpen}>
+          Agregar Alergia
+        </Button>
       </div>
-      <Button color='primary' endContent={<Add size={18} />}>
-        Agregar Alergia
-      </Button>
-    </div>
+
+      <AllergyCreateModal isOpen={isOpen} onChange={onOpenChange} />
+    </>
   );
 }
