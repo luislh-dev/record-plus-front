25fa22523ca51e0b0f289af6f317cb21b2dd2ad7
Luis López <luislopezhuari27@gmail.com>
2025-07-12 19:42:24 -0500

    refactor(DrawerFileViewer): extraer lógica de renderizado de archivos a una función para mejorar la legibilidad

--- src/components/DrawerFileViewer.tsx
+++ src/components/DrawerFileViewer.tsx
@@ -10,23 +10,25 @@ interface DrawerFileViewerProps {
 }
 
 export const DrawerFileViewer = ({ isOpen, onClose, url, mineType }: DrawerFileViewerProps) => {
+  const renderFileContent = () => {
+    if (mineType === 'application/pdf') {
+      return <PDFViewer src={url} />;
+    }
+
+    if (mineType.startsWith('image/')) {
+      return <ImageViewer src={url} controlSize='sm' />;
+    }
+
+    return <div>Tipo de archivo no permitido</div>;
+  };
+
   return (
-    <>
-      <Drawer isOpen={isOpen} onClose={onClose} size='5xl' isDismissable={false}>
-        <DrawerContent>
-          <DrawerBody>
-            <div className='pt-8'>
-              {mineType === 'application/pdf' ? (
-                <PDFViewer src={url} />
-              ) : mineType.startsWith('image/') ? (
-                <ImageViewer src={url} controlSize='sm' />
-              ) : (
-                <div>Tipo de archivo no permitido</div>
-              )}
-            </div>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </>
+    <Drawer isOpen={isOpen} onClose={onClose} size='5xl' isDismissable={false}>
+      <DrawerContent>
+        <DrawerBody>
+          <div className='pt-8'>{renderFileContent()}</div>
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
   );
 };
